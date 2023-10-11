fd759759217c37c6e83ba410396d3425be1f7038
haris-peter <luccaibrhim13@gmail.com>
2023-10-11 21:25:32 +0530

    first commit

---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,8 @@
+
+String loginUrlImage = 'https://img.freepik.com/free-vector/cartoon-galaxy-background_23-2148984169.jpg';
+String signUpUrlImage = 'https://img.freepik.com/free-vector/cartoon-galaxy-background_23-2148984169.jpg';
+String forgeUrlImage = 'https://images.pexels.com/photos/5496469/pexels-photo-5496469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+String? name = '';
+String? userImage = '';
+String? location = '';
+
